b4109f423a0d5f30b4a404b40cf244abde62672f
matheuspolycarpo <matheuspolycarpo@gmail.com>
2025-05-12 19:20:51 -0300

    projeto final

--- projeto.js
+++ projeto.js
@@ -1,24 +1,24 @@
-let xp= 80009;
-let nome= "matheus";
+let xp= 9;
+let nome= "clerio";
 
 if (xp <=  1000){
-  console.log("fERRO")
+  console.log("O heroi de nome " +nome+ " esta no nivel Ferro")
   } 
   else if (xp>= 1001 && xp<=2000){
-  console.log("Bronze")
+  console.log("O heroi de nome " +nome+ " esta no nivel Bronze")
   }
   else if(xp >= 2001 && xp<=5000){
-  console.log("Prata")
+  console.log("O heroi de nome " +nome+ " esta no nivel Prata")
   }
   else if (xp >= 5001 && xp<=7000){
-  console.log("Ouro")
+  console.log("O heroi de nome " +nome+ " esta no nivel Ouro")
   }
   else if (xp >= 7001 && xp<=8000){
-  console.log("Platina")
+  console.log("O heroi de nome " +nome+ " esta no nivel Platina")
   }
   else if (xp>= 8001 && xp<=9001){
-  console.log("Ascendente")
+  console.log("O heroi de nome " +nome+ " esta no nivel Ascendente")
   }
   else if (xp>10000){
-  console.log("Radiante")
+  console.log("O heroi de nome " +nome+ " esta no nivel Radiante")
   }
