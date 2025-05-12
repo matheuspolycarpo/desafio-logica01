7dec82615243043f8eaeb44e5141e187a3162029
matheuspolycarpo <matheuspolycarpo@gmail.com>
2025-05-12 19:03:07 -0300

    Create projeto.js

---
+++ projeto.js
@@ -0,0 +1,24 @@
+let xp= 80009;
+let nome= "matheus";
+
+if (xp <=  1000){
+  console.log("fERRO")
+  } 
+  else if (xp>= 1001 && xp<=2000){
+  console.log("Bronze")
+  }
+  else if(xp >= 2001 && xp<=5000){
+  console.log("Prata")
+  }
+  else if (xp >= 5001 && xp<=7000){
+  console.log("Ouro")
+  }
+  else if (xp >= 7001 && xp<=8000){
+  console.log("Platina")
+  }
+  else if (xp>= 8001 && xp<=9001){
+  console.log("Ascendente")
+  }
+  else if (xp>10000){
+  console.log("Radiante")
+  }
